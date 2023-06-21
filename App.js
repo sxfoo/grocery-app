882e2f65ea684fcf3299124ace79251205f068ec
wjunjie01 <junjiework48@gmail.com>
2023-06-21 18:40:05 +0800

    something

--- App.js
+++ App.js
@@ -68,6 +68,6 @@ export default function App() {
 
       </NavigationContainer>
 
-    </PaperProvider >
+    </PaperProvider>
   );
 }
--- screens/Trial.js
+++ screens/Trial.js
@@ -1,46 +1,108 @@
-import React from "react"
-import {Image, View, Text, StyleSheet} from 'react-native'
-import { BorderRadiuses } from "react-native-ui-lib";
+import React, {useState} from "react";
+import {View, Text, StyleSheet} from "react-native";
+import {FontAwesome} from '@expo/vector-icons';
+import {Dropdown} from 'react-native-element-dropdown'
 
 const style = StyleSheet.create({
-    greeting: {
-    fontFamily: 'Roboto',
-    color: 'white',
-    fontSize: 30,
-    fontWeight: 'bold'
-},
     container: {
-        display: 'flex',
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#161818',
+        borderColor: 'green',
+        borderWidth: 1,
+    },
+    header: {
         flexDirection: 'row',
-        alignItems: 'flex-start',
-        padding: 0,
-        position: 'absolute',
-        left: 14,
-        top: 32
-    }
+        justifyContent: 'space-between',
+    },
+
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    user: {
+        fontWeight:'medium',
+        color: 'grey',
+        fontSize: 20,
+    },
+    recent_exp: {
+        fontWeight: 'medium',
+        color: 'white',
+        fontSize: 28,
+    },
+    dropdown: {
+        margin: 16,
+        height: 50,
+        width: 150,
+        backgroundColor: '#EEEEEE',
+        borderRadius: 22,
+        paddingHorizontal: 8,
+      },
+      imageStyle: {
+        width: 24,
+        height: 24,
+        borderRadius: 12,
+      },
+      placeholderStyle: {
+        fontSize: 16,
+      },
+      selectedTextStyle: {
+        fontSize: 16,
+        marginLeft: 8,
+      },
+    
 });
 
-const avatar = {
-    uri: 'https://cdn3.vectorstock.com/i/1000x1000/30/97/flat-business-man-user-profile-avatar-icon-vector-4333097.jpg',
-    width: 60,
-    height: 60,
-};
 
 const Profile = () => {
     return(
-        <View style={style.container}>
-            <Text style={style.greeting}> Good Morning </Text>
-            <Image style = {{borderRadius: 50}} source = {avatar}/>
+        <View style = {style.header}> 
+            <View> 
+                <Text style={style.title}>Hello, </Text>
+                <Text style = {style.user}>User </Text>
+            </View>
+            <FontAwesome name="user-circle" size = {45} color='gray'/>    
         </View>
+    )
+};
+
+const Recent_expense = () =>{
+    const [selected, setSelected] = useState(""); /* default select is None*/
+
+    const data = [
+        {label:'Recent', value: '1'},
+        {label:'Ascending order', value: '2'},
+        {label:'Descending order', value: '3'},
+    ];
+
+    return(
+        <Dropdown
+            data = {data}
+            style = {style.dropdown}
+            selectedTextStyle = {style.selectedTextStyle}
+            placeholderStyle = {style.placeholderStyle}
+            maxHeight={200}
+            value = "selected"
+            valueField="value"
+            labelField="label"
+            placeholder="Recent"
+            onChange={item => {setSelected(item.value)}}
+
+        />
     )
 };
 
 
 const TrialScreen = () => {
     return(
-        <View>
+        <View style = {style.container}>
             <Profile/>
+            <Text style = {{fontSize: 48}}>Insert graph here </Text>
+            <Recent_expense/> 
         </View>
+
+        
     )
 };
 
