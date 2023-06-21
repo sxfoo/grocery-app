c21336a6578f89af193641e71c8bba7b1f491f15
sxfoo <fsx159@gmail.com>
2023-06-21 13:57:34 +0800

    Added BottomTab Nav, Added ListHeader and some placeholder text for the List

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from './screens/AllLists';
 import ListScreen from './screens/List';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function ListStack() {
+  return (
+    /* Stack Navigation for displaying Grocery List. Refer to screens directory */
+    <Stack.Navigator>
+      <Stack.Screen name="All Lists" component={HomeScreen} />
+      <Stack.Screen name="List" component={ListScreen} />
+    </Stack.Navigator >
+  );
+}
 
 export default function App() {
 
@@ -24,21 +37,33 @@ export default function App() {
   }
 
   return (
-
     /* React Native Paper requirements */
     <PaperProvider>
 
       {/* React Native Navigation container. For Stack, Tabs, Drawer navigation */}
       <NavigationContainer theme={CustomTheme}>
 
-        {/* Stack Navigation for displaying Grocery List. Refer to screens directory */}
-        <Stack.Navigator>
-          <Stack.Screen name="All Lists" component={HomeScreen} />
-          <Stack.Screen name="List" component={ListScreen} />
-        </Stack.Navigator>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarHideOnKeyboard: Platform.OS!== 'ios',
+              tabBarStyle: { position: 'absolute' }
+            }}>
+
+            <Tab.Screen
+              name="Grocery List"
+              component={ListStack}
+              options={{
+                headerShown: false,
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons name="view-list" color={color} size={size} />
+                ),
+                title: 'Grocery List'
+              }} />
+
+          </Tab.Navigator>
 
       </NavigationContainer>
 
-    </PaperProvider>
+    </PaperProvider >
   );
 }
--- screens/List.js
+++ screens/List.js
@@ -1,9 +1,51 @@
 import { useCallback, useMemo, useRef, useState } from 'react';
-import { Text, View, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import BottomSheet, { BottomSheetTextInput, BottomSheetBackdrop, BottomSheetView, BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import { Text, View, StyleSheet, TouchableWithoutFeedback, Keyboard, Pressable, Switch, Touchable } from 'react-native';
+import BottomSheet, { BottomSheetTextInput, BottomSheetBackdrop, BottomSheetView } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useTheme } from '@react-navigation/native';
-import { IconButton, Switch } from 'react-native-paper';
+import { IconButton, Divider } from 'react-native-paper';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+
+/* Displays Title of the List + User's Location */
+const ListHeader = () => {
+
+  return (
+    <View style={{ flexDirection: 'column', gap: 20 }}>
+
+      <Text style={[styles.headerText, { fontSize: 28 }]}> Home </Text>
+
+      <Pressable onPress={() => console.log('location pressed')} style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <IconButton
+          icon='map-marker-radius'
+          iconColor='#3E4C59'
+          containerColor='#E4E7EB'
+          mode='contained'
+          size={32}
+        />
+
+        <View style={{ flexDirection: 'column' }}>
+          <Text style={[styles.textOptions, { fontSize: 14, color: 'rgb(199, 199, 204)' }]}> Nearest Supermarket </Text>
+          <Text style={styles.textOptions}> Fairprice Xtra @ Ang Mo Kio Street.. </Text>
+        </View>
+
+      </Pressable>
+
+      <Divider />
+
+    </View>
+  )
+}
+
+/* Renders List Items. No logic currently */
+const ListOfItems = () => {
+
+  return (
+    <View style={{ marginTop: 50, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 40 }}>
+      <Text style={styles.textOptions}> You haven’t added any items. </Text>
+      <Text style={[styles.textOptions, { textAlign: 'center' }]}> Tap to add items that you need from your vendor </Text>
+    </View>
+  )
+}
 
 /* The overall Screen to be displayed. */
 const App = () => {
@@ -11,8 +53,10 @@ const App = () => {
   const { colors } = useTheme();
   const bottomSheetRef = useRef(null);
 
+  const bottomTabHeight = useBottomTabBarHeight();
+
   /* The % of screen the bottom Sheet should snap to */
-  const snapPoints = useMemo(() => ['15%', '90%'], []);
+  const snapPoints = useMemo(() => [bottomTabHeight + 100, '90%'], []);
 
   /* This is not needed. Currently logs the position which the bottomSheet snaps */
   const handleSheetChanges = useCallback((index) => {
@@ -25,7 +69,8 @@ const App = () => {
     (props) => (
       <BottomSheetBackdrop
         {...props}
-        pressBehavior={'collapse'} />
+        pressBehavior={'collapse'}
+      />
     ),
     []
   );
@@ -38,71 +83,76 @@ const App = () => {
 
     /* Used for react native gesture handler */
     <GestureHandlerRootView style={{ flex: 1 }}>
-    
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 
-        {/* The overall screen. */}
-        <View style={styles.body(colors)}>
+      <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
 
-          <Text style={[styles.headerText, {fontSize: 28}]}> Home </Text>
+          {/* The overall screen. */}
+          <View style={styles.body(colors)}>
 
-          {/* The Bottom Sheet. */}
-          <BottomSheet
-            ref={bottomSheetRef}
-            index={0}
-            snapPoints={snapPoints}
-            onChange={handleSheetChanges}
-            backgroundStyle={styles.sheetBg(colors)}
-            backdropComponent={renderBackdrop}
-            keyboardBehavior='extend'
-          >
+            <ListHeader />
+            <ListOfItems />
 
-            <BottomSheetView style={styles.contentContainer}>
-              
-              {/* The row containing the text input */}
-              <BottomSheetView style={styles.row}>
-                <BottomSheetTextInput
-                  style={styles.searchbar}
-                  placeholder='e.g. Milk'
-                  placeholderTextColor='rgba(199, 199, 204, 0.4)' />
-                <IconButton
-                  icon='plus-thick'
-                  iconColor='#3E4C59'
-                  containerColor='#E4E7EB'
-                  mode='contained'
-                  size={28}
-                />
+            {/* The Bottom Sheet. */}
+            <BottomSheet
+              ref={bottomSheetRef}
+              index={0}
+              snapPoints={snapPoints}
+              onChange={handleSheetChanges}
+              backgroundStyle={styles.sheetBg(colors)}
+              backdropComponent={renderBackdrop}
+              keyboardBehavior='extend'
+            >
+
+              <BottomSheetView style={styles.contentContainer}>
+
+                {/* The row containing the text input */}
+                <BottomSheetView style={styles.row}>
+                  <BottomSheetTextInput
+                    style={styles.searchbar}
+                    placeholder='e.g. Milk'
+                    placeholderTextColor='rgba(199, 199, 204, 0.4)' />
+                  <IconButton
+                    icon='plus-thick'
+                    iconColor='#3E4C59'
+                    containerColor='#E4E7EB'
+                    mode='contained'
+                    size={28}
+                  />
+                </BottomSheetView>
+
+                {/* The additional options to be carried out. Currently no logic.*/}
+                <BottomSheetView style={[styles.row, { marginTop: 60, justifyContent: 'space-between' }]}>
+                  <Text style={styles.headerText}> Additional Item Details </Text>
+                </BottomSheetView>
+
+                <BottomSheetView style={[styles.row, { marginTop: 32, justifyContent: 'space-between' }]}>
+                  <Text style={styles.textOptions}> Category </Text>
+                  <Switch
+                    trackColor={{ false: '#767577', true: 'green' }}
+                    thumbColor={'#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={() => { setCategorySwitch(!categorySwitch) }}
+                    value={categorySwitch}
+                  />
+                </BottomSheetView>
+
+                <BottomSheetView style={[styles.row, { marginTop: 32, justifyContent: 'space-between' }]}>
+                  <Text style={styles.textOptions}> Quantity </Text>
+                  <Switch
+                    trackColor={{ false: '#767577', true: 'green' }}
+                    thumbColor={'#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={() => { setQuantitySwitch(!quantitySwitch) }}
+                    value={quantitySwitch}
+                  />
+                </BottomSheetView>
+
               </BottomSheetView>
-              
-              {/* The additional options to be carried out. Currently no logic.*/}
-              <BottomSheetView style={[styles.row, {marginTop: 60, justifyContent: 'space-between'}]}>
-                <Text style={styles.headerText}> Additional Item Details </Text>
-              </BottomSheetView>
 
-              <BottomSheetView style={[styles.row, {marginTop: 32, justifyContent: 'space-between'}]}>
-                <Text style={styles.textOptions}> Category </Text>
-                <Switch 
-                value={categorySwitch} 
-                onValueChange={() => {setCategorySwitch(!categorySwitch)}} 
-                color='#63c064'
-                />
-              </BottomSheetView>
+            </BottomSheet>
 
-              <BottomSheetView style={[styles.row, {marginTop: 32, justifyContent: 'space-between'}]}>
-                <Text style={styles.textOptions}> Quantity </Text>
-                <Switch 
-                value={quantitySwitch} 
-                onValueChange={() => {setQuantitySwitch(!quantitySwitch)}} 
-                color='#63c064'
-                />
-              </BottomSheetView>
+          </View>
 
-            </BottomSheetView>
-
-          </BottomSheet>
-
-        </View>
-        
       </TouchableWithoutFeedback>
 
     </GestureHandlerRootView>
@@ -112,7 +162,7 @@ const App = () => {
 const styles = StyleSheet.create({
   body: (theme) => ({
     flex: 1,
-    padding: 12,
+    padding: 16,
     backgroundColor: theme.background,
   }),
 
@@ -131,13 +181,14 @@ const styles = StyleSheet.create({
     marginRight: 8,
     padding: 16,
     borderRadius: 12,
-    fontSize: 18,
+    fontSize: 16,
+    height: 50,
     backgroundColor: '#1f2933',
     color: '#f5f7fa',
   },
 
   row: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     alignItems: 'center'
   },
 
